495a31a161f059589a9996dd8cae90f66eb80f32
Joyce Silva <joycesilva.web@gmail.com>
2016-09-25 19:25:39 -0300

    Add cor para o miniTeste e correção de sintaxe

--- MiniTeste.rb
+++ MiniTeste.rb
@@ -1,10 +1,11 @@
 require 'MiniTeste/autorun'
+require 'MiniTeste/pride'
 require './furlong'
 
 class MiniTeste
 
     def test_one_furlong
         km = Furlong.new.furlong_to_kilometers(1)
-        assert_delta 0.201168, km, 0.001
+        assert_in_delta 0.201168, km, 0.001
     end
 end
