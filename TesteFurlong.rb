52ef486d38855fefb1c355e9a5bb4920ce88e071
Joyce Silva <joycesilva.web@gmail.com>
2016-09-25 17:38:50 -0300

    Convertendo outros valores de furlongs em Km

--- TesteFurlong.rb
+++ TesteFurlong.rb
@@ -7,4 +7,10 @@ describe Furlong do
 		km = calc.furlong_to_kilometers(1)
 		km.should be_within(0.0001).of(0.201168)
 	end
+
+	it "converts a maraton: 209.75 furlongs to 42.194988 km" do
+		calc = Furlong.new
+		km = calc.furlong_to_kilometers(209.75)
+		km.should be_within(0.001).of(42.194988)
+	end
 end
