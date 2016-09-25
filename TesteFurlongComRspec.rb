25b523e7561a3529a37d1da0475e956bb046e477
Joyce Silva <joycesilva.web@gmail.com>
2016-09-25 18:28:43 -0300

    Refactor do Teste, criando variavel local

--- TesteFurlongComRspec.rb
+++ TesteFurlongComRspec.rb
@@ -2,15 +2,14 @@ require 'rspec'
 require './furlong'
 
 describe Furlong do
+
+	let(:calculator){ Furlong.new}
+
 	it "converts 1 furlong to  0.201168 km" do
-		calc = Furlong.new
-		km = calc.furlong_to_kilometers(1)
-		km.should be_within(0.0001).of(0.201168)
+		calculator.furlong_to_kilometers(1).should be_within(0.0001).of(0.201168)
 	end
 
 	it "converts a maraton: 209.75 furlongs to 42.194988 km" do
-		calc = Furlong.new
-		km = calc.furlong_to_kilometers(209.75)
-		km.should be_within(0.001).of(42.194988)
+		calculator.furlong_to_kilometers(209.75).should be_within(0.001).of(42.194988)
 	end
 end
